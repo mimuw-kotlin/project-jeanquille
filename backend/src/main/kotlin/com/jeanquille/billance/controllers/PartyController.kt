779d09c38427fafe4747cc5249e546fac86d88aa
Zonik <stasio.struzik@gmail.com>
2024-12-08 21:40:29 +0100

    smth

--- backend/src/main/kotlin/com/jeanquille/billance/controllers/PartyController.kt
+++ backend/src/main/kotlin/com/jeanquille/billance/controllers/PartyController.kt
@@ -1,4 +1,41 @@
 package com.jeanquille.billance.controllers
 
-class PartyController {
+import com.jeanquille.billance.models.Account
+import com.jeanquille.billance.models.Party
+import com.jeanquille.billance.services.PartyService
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+import java.util.*
+
+
+@RestController
+class PartyController (private val partyService: PartyService) {
+
+    @GetMapping("/parties/{accountId}")
+    fun getParties(@PathVariable accountId: UUID): List<Party> = partyService.getPartiesByAccountId(accountId)
+
+    @GetMapping("/party/{partyId}")
+    fun getParty(@PathVariable partyId: Long): Party = partyService.getPartyById(partyId)
+
+    @GetMapping("/parties")
+    fun getAllParties(): List<Party> = partyService.getAllParties()
+
+    @PutMapping("/party/{partyId}/name")
+    fun updatePartyName(@PathVariable partyId: Long, @RequestBody json: Map<String, String>): Party {
+        return partyService.updateParty(partyId, json["name"]!!)
+    }
+
+    @DeleteMapping("/party/{partyId}")
+    fun deleteParty(@PathVariable partyId: Long) {
+        partyService.deleteParty(partyId)
+    }
+
+    @PostMapping("/party")
+    fun createParty(creator: Account): Party {
+        val party = Party()
+        party.name = "New Party"
+        party.date = LocalDateTime.now()
+
+        return partyService.createParty(party, creator)
+    }
 }
--- backend/src/main/kotlin/com/jeanquille/billance/services/PartyService.kt
+++ backend/src/main/kotlin/com/jeanquille/billance/services/PartyService.kt
@@ -1,12 +1,55 @@
 package com.jeanquille.billance.services
 
+import com.jeanquille.billance.models.Account
+import com.jeanquille.billance.models.Member
 import com.jeanquille.billance.models.Party
 import com.jeanquille.billance.repositories.PartyRepository
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+import java.util.UUID
 
 @Service
 class PartyService(val partyRepository: PartyRepository) {
     fun getAllParties(): MutableList<Party> {
         return partyRepository.findAll()
+    }
+
+    fun getPartiesByAccountId(accountId: UUID): MutableList<Party> {
+        return partyRepository.findByMembersAccountId(accountId)
+    }
+
+    fun getPartyById(partyId: Long): Party {
+        return partyRepository.findById(partyId).orElseThrow()
+    }
+
+    fun createParty(party: Party, creator: Account): Party {
+        val member = Member()
+        member.account.id = creator.id
+        party.members.add(member)
+
+        return partyRepository.save(party)
+    }
+
+    fun updateParty(partyId: Long, newPartyName: String): Party {
+        val party = partyRepository.findById(partyId).orElseThrow()
+        party.name = newPartyName
+        return partyRepository.save(party)
+    }
+
+    fun deleteParty(partyId: Long) {
+        partyRepository.deleteById(partyId)
+    }
+
+    fun addMember(partyId: Long, accountId: UUID) : Party {
+        val party = partyRepository.findById(partyId).orElseThrow()
+        if (party.members.any { it.account.id == accountId }) {
+            throw ResponseStatusException(HttpStatus.FORBIDDEN, "Account is already a member of this party")
+        }
+        val member = Member()
+        member.account.id = accountId
+        party.members.add(member)
+
+        return partyRepository.save(party)
     }
 }
