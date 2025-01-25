1d890c3b5bf3cbd1b4a573cb6d86f93df77dae56
Zonik <stasio.struzik@gmail.com>
2025-01-25 16:35:03 +0100

    Removed friends from json

--- backend/src/main/kotlin/com/jeanquille/billance/models/Account.kt
+++ backend/src/main/kotlin/com/jeanquille/billance/models/Account.kt
@@ -1,10 +1,7 @@
 package com.jeanquille.billance.models
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo
 import com.fasterxml.jackson.annotation.JsonIgnore
-import com.fasterxml.jackson.annotation.JsonManagedReference
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.annotation.ObjectIdGenerators
 import jakarta.persistence.*
 import java.util.*
 
